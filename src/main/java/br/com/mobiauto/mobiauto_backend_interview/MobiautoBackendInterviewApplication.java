4e51a1cfcc047d55b96745d255fed007f6a55b55
Marcus Fábio Salviano <marcussalviano@gmail.com>
2024-06-25 08:59:39 -0400

    Initial Files

---
+++ src/main/java/br/com/mobiauto/mobiauto_backend_interview/MobiautoBackendInterviewApplication.java
@@ -0,0 +1,13 @@
+package br.com.mobiauto.mobiauto_backend_interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobiautoBackendInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobiautoBackendInterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mobiauto/mobiauto_backend_interview/MobiautoBackendInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mobiauto.mobiauto_backend_interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobiautoBackendInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
